69421ece12cdc1404fc9c1dc738e2bd1822d3d21
takaki.ishibashi2 <taka@reaktor.co.jp>
2018-01-22 01:24:36 +0900

    logger: Ingect runEnv value into log-level

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,13 @@
 import * as winston from "winston"
+import { config } from "./config"
+
+const levelValue = config.runEnv === 'production' ? 'warn' : 'debug'
 
 export const logger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({
-        "timestamp": true,
-	"level": "debug",
+      "timestamp": true,
+      "level": levelValue,
       })
   ],
 })
