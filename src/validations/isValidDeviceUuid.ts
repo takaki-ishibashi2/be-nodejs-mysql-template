6e7cc140e77e74da59e6f7bbf8536c52e50de9c3
takaki.ishibashi2 <taka@reaktor.co.jp>
2018-01-23 00:33:38 +0900

    src/validations: Be short

--- src/validations/isValidDeviceUuid.ts
+++ src/validations/isValidDeviceUuid.ts
@@ -1,12 +1,9 @@
 import { DEVICE_UUID_LENGTH } from '../constans'
 
 export const isValidDeviceUuid = (uuid: string) => {
-  if (
-    uuid
+  return (
+    !!uuid
     && typeof uuid !== 'undefined'
     && uuid.length === DEVICE_UUID_LENGTH
-  ) {
-    return true
-  }
-  return false
+  )
 }
