bbb6e9fc5bd961e0fdb41235568be3f0c2da0d6c
takaki.ishibashi2 <taka@reaktor.co.jp>
2018-01-23 00:41:47 +0900

    src/routes/device: Remove extra dot

--- src/routes/device.ts
+++ src/routes/device.ts
@@ -17,7 +17,7 @@ export const deviceRoute = (server: express.Application) => {
           const validated = validations.validateRequestBodyOfPostingDevice(req.body)
 
           if(!validations.isValidDeviceUuid(validated.uuid)) {
-            return modules.sendErrorMessage(res, 400, 'Bad request.')
+            return modules.sendErrorMessage(res, 400, 'Bad request')
           } else {
             database.insertOrUpdateDevice(validated.uuid, validated.model, validated.os, validated.date)
               .then(() => { return sendErrorMessage(res, 200, '') })
@@ -27,11 +27,11 @@ export const deviceRoute = (server: express.Application) => {
               })
           }
         } else {
-          return modules.sendErrorMessage(res, 403, 'Access not allowed.')
+          return modules.sendErrorMessage(res, 403, 'Access not allowed')
         }
       } catch (err) {
         logger.error(`Issue handling ${path}`, err, req.headers, req.body);
-        return modules.sendErrorMessage(res, 400, 'Bad request.')
+        return modules.sendErrorMessage(res, 400, 'Bad request')
       }
     })
     .get((req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -40,26 +40,26 @@ export const deviceRoute = (server: express.Application) => {
           const validated = validations.validateRequestBodyOfGettingDevice(req)
 
           if (!validations.isValidDeviceUuid(validated.uuid)) {
-            return modules.sendErrorMessage(res, 400, 'Bad request.')
+            return modules.sendErrorMessage(res, 400, 'Bad request')
           } else {
             database.selectDevice(validated.uuid)
               .then(result => {
                 if (!Array.isArray(result) || result.length == 0) {
-                  return modules.sendErrorMessage(res, 400, 'Bad request.')
+                  return modules.sendErrorMessage(res, 400, 'Bad request')
                 } else {
                   return modules.sendSuccessMessage(res, 200, JSON.stringify(result))
                 }
               })
               .catch(err => {
-                return modules.sendErrorMessage(res, 500, 'Internal error.')
+                return modules.sendErrorMessage(res, 500, 'Internal error')
               })
           }
         } else {
-          return modules.sendErrorMessage(res, 403, 'Access not allowed.')
+          return modules.sendErrorMessage(res, 403, 'Access not allowed')
         }
       } catch (err) {
         logger.error(`Issue handling ${path}`, err, req.headers, req.body)
-        return modules.sendErrorMessage(res, 403, 'Access not allowed.')
+        return modules.sendErrorMessage(res, 403, 'Access not allowed')
       }
     })
 }
