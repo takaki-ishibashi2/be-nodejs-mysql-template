0cf8d03acd57c2ea20072af0b87dc376eacbd02a
takaki.ishibashi2 <taka@reaktor.co.jp>
2018-01-22 20:41:09 +0900

    src/routes: Be short the response message

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,6 +3,6 @@ import * as express from 'express'
 export const homeRoute = (server: express.Application) => {
   server.route(`/`)
   .get((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.status(200).end('Awesome software!')
+    res.status(200).end('Awesome!')
   })
 }
