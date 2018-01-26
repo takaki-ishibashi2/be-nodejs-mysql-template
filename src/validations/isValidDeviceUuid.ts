6bbba84aa058c92c9e55e3028319d5606980feaa
takaki.ishibashi2 <taka@reaktor.co.jp>
2018-01-27 00:12:58 +0900

    Implement: Escape for uuid

--- src/validations/isValidDeviceUuid.ts
+++ src/validations/isValidDeviceUuid.ts
@@ -1,9 +1,11 @@
+import * as escape from 'escape-html'
 import { DEVICE_UUID_LENGTH } from '../constans'
 
 export const isValidDeviceUuid = (uuid: string) => {
+  const escaped = escape(uuid)
   return (
-    !!uuid
-    && typeof uuid !== 'undefined'
-    && uuid.length === DEVICE_UUID_LENGTH
+    !!escaped
+    && typeof escaped !== 'undefined'
+    && escaped.length === DEVICE_UUID_LENGTH
   )
 }
