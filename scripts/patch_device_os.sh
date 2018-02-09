49636811cc8132af94833ce6e8e107d154dbd2e6
takaki.ishibashi2 <taka@reaktor.co.jp>
2018-02-09 14:29:10 +0900

    Implement: Add new curl script for patch endpoint

---
+++ scripts/patch_device_os.sh
@@ -0,0 +1,1 @@
+curl -H "Content-type: application/json" -H "X-API-Key: m3FUxbLExcUqxpYw" -X PATCH -i "localhost:8081/api/v1/devices/1273F2826CFD4D2D8243EE63654320BA/os/iOS/12.0.4/2018-01-10T22:26:45.167" -u admin:admin
