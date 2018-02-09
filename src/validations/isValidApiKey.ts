0a16addb4c587f861c10b8912fa15c474fb7ef69
takaki.ishibashi2 <taka@reaktor.co.jp>
2018-02-09 14:28:38 +0900

    refact: Add empty line to isValidApiKey

--- src/validations/isValidApiKey.ts
+++ src/validations/isValidApiKey.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import { config } from '../config'
+
 export const isValidApiKey = (req: express.Request) => {
   const providedKey = req.header("X-API-Key")
   return providedKey === config.authKey
